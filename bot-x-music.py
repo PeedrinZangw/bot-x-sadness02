08a15eb5aea7abbc77c9324e46e1d27972e8ce7d
PeedrinZangw <39870039+PeedrinZangw@users.noreply.github.com>
2018-08-18 03:53:52 +0000

    Add files via upload

--- bot-x-music.py
+++ bot-x-music.py
@@ -1,34 +1,124 @@
 import discord
-import youtube_dl
 from discord.ext import commands
-
-TOKEN = 'NDczMjUwOTI5MDg5NDQ1OTE4.Dj__HA.2wpr3x3ybqwBerCZ0w9qPYxoKTI'
-client = commands.Bot(command_prefix = 'rwdj!')
+#VOCÊ PRECISA DO YOUTUBE-DL INSTALADO, PARA ISSO DIGITE "pip install youtube_dl" NO CMD!
+client = discord.Client()
 
 players = {}
+COR = 0xF7FE2E
 
 @client.event
 async def on_ready():
-    print('Ratazanas bot - Online')
-    await client.change_presence(game=discord.Game(name='created by ! Ｈａｚａｒｄ 竹エッ#0459'))
+    print(client.user.name)
+    print("===================")
+    await client.change_presence(game=discord.Game(name="0/32 Players Online no SupremeCS! IP: [insira IP aqui!]", url="https://twitch.tv/SupremeCSstreaming", type=1))
 
-@client.command(pass_context=True)
-async def join(ctx):
-    channel = ctx.message.author.voice.voice_channel
-    await client.join_voice_channel(channel)
+@client.event
+async def on_member_join(member):
+  msg = "Olá, Bem-vindo ao ```SᴜᴘʀᴇᴍᴇCS.ɴᴇᴛ ★ Sᴜᴘᴘᴏʀᴛ Sᴇʀᴠᴇʀ```\nAdicione nossos servidores nos favoritos!\n\n```[4FUN]: insira IP aqui!\n[CS:GO MOD]: insira IP aqui!```\nVisite também nosso site: ```www.SupremeCS.net```\n\nCaso tem algum amigo que também joga no SupremeCS que deseja convidar ao servidor, use este link: https://discord.gg/cPbychK"
+  await client.send_message(member, msg)
+  role = discord.utils.get(member.server.roles, name='★ Supreme Member ★')
+  await client.add_roles(member, role)
 
-@client.command(pass_context=True)
-async def leave(ctx):
-    server = ctx.message.server
-    voice_client = client.voice_client_in(server)
-    await voice_client.disconect()
+@client.event
+async def on_message(message):
+    if message.content.startswith('!entrar'):
+        try:
+            channel = message.author.voice.voice_channel
+            await client.join_voice_channel(channel)
+        except discord.errors.InvalidArgument:
+            await client.send_message(message.channel, "O bot ja esta em um canal de voz")
+        except Exception as error:
+            await client.send_message(message.channel, "Ein Error: ```{error}```".format(error=error))
 
-@client.command(pass_context=True)
-async def play (ctx, url):
-    server = ctx.message.server
-    voice_client = client.voice_client_in(server)
-    player = await voice_client.create_ytdl_player(url)
-    players[server.id] = player
-    player.start()
+    if message.content.startswith('!sair'):
+        try:
+            mscleave = discord.Embed(
+                title="\n",
+                color=COR,
+                description="Sai do canal de voz e a musica parou!"
+            )
+            voice_client = client.voice_client_in(message.server)
+            await client.send_message(message.channel, embed=mscleave)
+            await voice_client.disconnect()
+        except AttributeError:
+            await client.send_message(message.channel, "O bot não esta em nenhum canal de voz.")
+        except Exception as Hugo:
+            await client.send_message(message.channel, "Ein Error: ```{haus}```".format(haus=Hugo))
 
-client.run(TOKEN)
+    if message.content.startswith('!play'):
+        try:
+            yt_url = message.content[6:]
+            if client.is_voice_connected(message.server):
+                try:
+                    voice = client.voice_client_in(message.server)
+                    players[message.server.id].stop()
+                    player = await voice.create_ytdl_player('ytsearch: {}'.format(yt_url))
+                    players[message.server.id] = player
+                    player.start()
+                    mscemb = discord.Embed(
+                        title="Música para tocar:",
+                        color=COR
+                    )
+                    mscemb.add_field(name="Nome:", value="`{}`".format(player.title))
+                    mscemb.add_field(name="Visualizações:", value="`{}`".format(player.views))
+                    mscemb.add_field(name="Enviado em:", value="`{}`".format(player.uploaded_date))
+                    mscemb.add_field(name="Enviado por:", value="`{}`".format(player.uploadeder))
+                    mscemb.add_field(name="Duraçao:", value="`{}`".format(player.uploadeder))
+                    mscemb.add_field(name="Likes:", value="`{}`".format(player.likes))
+                    mscemb.add_field(name="Deslikes:", value="`{}`".format(player.dislikes))
+                    await client.send_message(message.channel, embed=mscemb)
+                except Exception as e:
+                    await client.send_message(message.server, "Error: [{error}]".format(error=e))
+
+            if not client.is_voice_connected(message.server):
+                try:
+                    channel = message.author.voice.voice_channel
+                    voice = await client.join_voice_channel(channel)
+                    player = await voice.create_ytdl_player('ytsearch: {}'.format(yt_url))
+                    players[message.server.id] = player
+                    player.start()
+                    mscemb2 = discord.Embed(
+                        title="Música para tocar:",
+                        color=COR
+                    )
+                    mscemb2.add_field(name="Nome:", value="`{}`".format(player.title))
+                    mscemb2.add_field(name="Visualizações:", value="`{}`".format(player.views))
+                    mscemb2.add_field(name="Enviado em:", value="`{}`".format(player.upload_date))
+                    mscemb2.add_field(name="Enviado por:", value="`{}`".format(player.uploader))
+                    mscemb2.add_field(name="Duraçao:", value="`{}`".format(player.duration))
+                    mscemb2.add_field(name="Likes:", value="`{}`".format(player.likes))
+                    mscemb2.add_field(name="Deslikes:", value="`{}`".format(player.dislikes))
+                    await client.send_message(message.channel, embed=mscemb2)
+                except Exception as error:
+                    await client.send_message(message.channel, "Error: [{error}]".format(error=error))
+        except Exception as e:
+            await client.send_message(message.channel, "Error: [{error}]".format(error=e))
+
+
+
+
+    if message.content.startswith('!pause'):
+        try:
+            mscpause = discord.Embed(
+                title="\n",
+                color=COR,
+                description="Musica pausada com sucesso!"
+            )
+            await client.send_message(message.channel, embed=mscpause)
+            players[message.server.id].pause()
+        except Exception as error:
+            await client.send_message(message.channel, "Error: [{error}]".format(error=error))
+    if message.content.startswith('!resume'):
+        try:
+            mscresume = discord.Embed(
+                title="\n",
+                color=COR,
+                description="Musica pausada com sucesso!"
+            )
+            await client.send_message(message.channel, embed=mscresume)
+            players[message.server.id].resume()
+        except Exception as error:
+            await client.send_message(message.channel, "Error: [{error}]".format(error=error))
+
+
+client.run('NDgwMTY2OTE0MDY1MTcwNDQx.Dlj2iw.omvUqgegRU6jrYlOGx9G4lVFv9I')
